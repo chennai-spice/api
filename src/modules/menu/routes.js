5f2887d238b24e29678bb58887b22829f8bcff36
Mohamed Jubair <amzubair@live.com>
2017-04-18 17:59:10 +0400

    BugFix: added '/' before each menu routes

--- src/modules/menu/routes.js
+++ src/modules/menu/routes.js
@@ -3,7 +3,7 @@ import * as Url from './controller'
 
 const menuRoute = new Router()
 
-menuRoute.post('v1/menu/new', Url.addMenuItem)
-menuRoute.get('v1/menu', Url.getAllMenuItems)
+menuRoute.post('/v1/menu/new', Url.addMenuItem)
+menuRoute.get('/v1/menu', Url.getAllMenuItems)
 
 export default menuRoute
